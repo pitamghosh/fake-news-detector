e5d32eec830cfbe435335a785c3dbb7b8862cd07
Pitam Ghosh <164029814+pitamghosh@users.noreply.github.com>
2026-02-26 19:39:43 +0530

    Update app.py

--- fake_news_ai_project/app.py
+++ fake_news_ai_project/app.py
@@ -15,9 +15,15 @@ st.set_page_config(
 )
 
 # -------------------- LOAD MODEL --------------------
-model = pickle.load(open("model.pkl", "rb"))
-vectorizer = pickle.load(open("vectorizer.pkl", "rb"))
+import os
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+model_path = os.path.join(BASE_DIR, "model.pkl")
+vectorizer_path = os.path.join(BASE_DIR, "vectorizer.pkl")
+
+model = pickle.load(open(model_path, "rb"))
+vectorizer = pickle.load(open(vectorizer_path, "rb"))
 # -------------------- STOPWORDS --------------------
 try:
     stop_words = set(stopwords.words('english'))
